ef6d1831ed3e8031ef39225d40099e84ce149284
zhuyst <13553371651@139.com>
2019-03-24 18:55:04 +0800

    feat(node-id-generator): 更换NodeId生成策略

--- node-id-generator/node_id_generator.go
+++ node-id-generator/node_id_generator.go
@@ -1,19 +1,29 @@
 package node_id_generator
 
 import (
+	"errors"
+	"fmt"
 	"github.com/go-redis/redis"
 	"github.com/zhuyst/redsync"
+	"time"
 )
 
 const (
-	NodeIdKey     = "SHORTURL_SERVICE:NODE_ID"
-	nodeIdLockKey = "SHORTURL_SERVICE:NODE_ID_LOCK"
+	nodeIdKeyPrefix = "SHORTURL_SERVICE:NODE_ID"
+	nodeIdLockKey   = "SHORTURL_SERVICE:NODE_ID_LOCK"
+
+	holdKeyTime = time.Minute * 5
 )
 
 type NodeIdGenerator struct {
+	NodeId int64
+
 	nodeMax     int64
 	redisClient *redis.Client
 	mutex       *redsync.Mutex
+
+	nodeIdKey  string
+	nodeHolder *time.Ticker
 }
 
 func New(redisClient *redis.Client, nodeMax int64) *NodeIdGenerator {
@@ -27,29 +37,63 @@ func New(redisClient *redis.Client, nodeMax int64) *NodeIdGenerator {
 }
 
 func (generator *NodeIdGenerator) Generate() (int64, error) {
-	err := generator.mutex.Lock()
-	if err != nil {
+	if err := generator.mutex.Lock(); err != nil {
 		return -1, err
 	}
-	redisClient := generator.redisClient
+	defer generator.mutex.Unlock()
 
-	nodeId, err := redisClient.Incr(NodeIdKey).Result()
-	if err != nil {
-		return -1, err
+	// 询问从0到nodeMax是否有坑位
+	var i int64
+	for i = 0; i < generator.nodeMax; i++ {
+		key := fmt.Sprintf("%s:%d", nodeIdKeyPrefix, i)
+
+		// 有Node占了坑位，跳过
+		err := generator.redisClient.Get(key).Err()
+		if err == nil {
+			continue
+		}
+
+		// 数据库错误直接返回
+		if err != redis.Nil {
+			return -1, err
+		}
+
+		// 开始生成NodeId
+		generator.NodeId = i
+		generator.nodeIdKey = key
+		if err := generator.startNodeHolder(); err != nil {
+			return -1, err
+		}
+		return generator.NodeId, nil
 	}
 
-	// 小于等于nodeMax可以直接返回
-	if nodeId <= generator.nodeMax {
-		generator.mutex.Unlock()
-		return nodeId, nil
+	return -1, fmt.Errorf("nodeNumber reached the maximum: %d", generator.nodeMax)
+}
+
+func (generator *NodeIdGenerator) startNodeHolder() error {
+	if generator.nodeIdKey == "" {
+		return errors.New("need nodeIdKey to startNodeHolder")
 	}
 
-	// 处理超过nodeMax的情况
-	nodeId = 0
-	if err = redisClient.Set(NodeIdKey, nodeId, 0).Err(); err != nil {
-		return -1, err
+	setFunc := func() error {
+		return generator.redisClient.Set(generator.nodeIdKey, true, holdKeyTime).Err()
 	}
 
-	generator.mutex.Unlock()
-	return nodeId, nil
+	renewTime := holdKeyTime - 30*time.Second
+	nodeHolder := time.NewTicker(renewTime)
+	generator.nodeHolder = nodeHolder
+
+	if err := setFunc(); err != nil {
+		return err
+	}
+
+	go func() {
+		for range nodeHolder.C {
+			if err := setFunc(); err != nil {
+				panic(err)
+			}
+		}
+	}()
+
+	return nil
 }
